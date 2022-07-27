59caf057f7f7c37b40eac02975fa08c19ff2af3d
Alex Jones <alexsimonjones@gmail.com>
2022-07-27 12:37:28 +0100

    bad cat

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	cd pkg && docker buildx build --platform linux/amd64,linux/arm64,linux/arm . -t cnskunkworks/cats:latest --push
+	cd pkg && docker buildx build --platform linux/amd64,linux/arm64,linux/arm . -t cnskunkworks/cats:bad-cat --push
 deploy:
 	helm upgrade --install cats .
 undeploy:
--- pkg/main.go
+++ pkg/main.go
@@ -12,14 +12,7 @@ var (
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
-	duration := time.Now().Sub(started)
-	if duration.Seconds() > 10 {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
-	} else {
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
-	}
+	w.WriteHeader(500)
 }
 
 func main() {
