eb00718da45768e9305a909faac9fb17d2470d0e
Alex Jones <alexsimonjones@gmail.com>
2022-07-27 12:33:16 +0100

    updated liveliness check

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
-	cd pkg && docker buildx build --platform linux/amd64,linux/arm64,linux/arm . -t tibbar/cat-cozy:latest --push
+	cd pkg && docker buildx build --platform linux/amd64,linux/arm64,linux/arm . -t cnskunkworks/cats:latest --push
 deploy:
-	helm upgrade --install cat-cozy .
+	helm upgrade --install cats .
+undeploy:
+	helm uninstall cats
--- pkg/main.go
+++ pkg/main.go
@@ -4,11 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
+var (
+	started = time.Now()
+)
+
+func health(w http.ResponseWriter, r *http.Request) {
+	duration := time.Now().Sub(started)
+	if duration.Seconds() > 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+}
+
 func main() {
+
 	fileServer := http.FileServer(http.Dir("./static")) // New code
-	http.Handle("/", fileServer)                        // New code
+	http.Handle("/", fileServer)
+	http.Handle("/healthz", http.HandlerFunc(health))
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
